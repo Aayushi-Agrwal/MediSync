c02fdfd02e9b4211b697c8784e01019b4d93d7c0
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-07-11 18:17:14 +0530

    feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,41 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/* Todo: OTP verfication/ passkey model */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="w-fit mb-12 h-10"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
 }
