18320ecd2b2dab5bda2eac1bd08f48e45da6c98f
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-07-16 11:55:25 +0530

    feat: appwrite config and create user

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import SubmitButton from "../ui/SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -42,16 +43,13 @@ const PatientForm = () => {
   }: z.infer<typeof UserFormValidation>) {
     setIsLoading(true);
     try {
-      // const userData = {
-      //   name,
-      //   email,
-      //   phone,
-      // };
-      // const user = await createUser(userData);
-      // if (user) {
-      //   router.push(`/patients/${user.$id}/register`);
-      // }
-      console.log(name, email, phone);
+      const userData = {
+        name,
+        email,
+        phone,
+      };
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.error(error);
     }
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,20 +1,26 @@
 import * as sdk from "node-appwrite";
+require("dotenv").config();
 
 export const {
-  PROJECT_ID,
-  API_KEY,
+  //   NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+  //   NEXT_PUBLIC_PROJECT_ID: PROJECT_ID,
+  //   NEXT_PUBLIC_API_KEY: API_KEY,
   DATABASE_ID,
   PATIENT_COLLECTION_ID,
   DOCTOR_COLLECTION_ID,
-  APPOINTMENTS_COLLECTION_ID,
+  APPOINTMENT_COLLECTION_ID,
   NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
 } = process.env;
+
+export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
+export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 const client = new sdk.Client();
 
 client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
+console.log(client);
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
 export const messaging = new sdk.Messaging(client);
