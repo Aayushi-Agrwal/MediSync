c70a50eacf84970923f428fdf546bf6249a3ff4a
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-07-25 20:38:42 +0530

    feat: create register form

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -5,22 +5,21 @@ export const {
   //   NEXT_PUBLIC_ENDPOINT: ENDPOINT,
   //   NEXT_PUBLIC_PROJECT_ID: PROJECT_ID,
   //   NEXT_PUBLIC_API_KEY: API_KEY,
-  DATABASE_ID,
+  // DATABASE_ID,
   PATIENT_COLLECTION_ID,
   DOCTOR_COLLECTION_ID,
   APPOINTMENT_COLLECTION_ID,
-  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
 } = process.env;
 
 export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
 export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export const DATABASE_ID = process.env.NEXT_PUBLIC_DATABASE_ID;
+export const BUCKET_ID = process.env.NEXT_PUBLIC_BUCKET_ID;
 
 const client = new sdk.Client();
 
 client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
-
-console.log(client);
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
 export const messaging = new sdk.Messaging(client);
